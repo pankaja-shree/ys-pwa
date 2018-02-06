45145a58ae2fce1899e542d3bacdec7d5bceb7e0
pankaja-shree <pankajashree@gmail.com>
2018-02-06 16:00:56 +0530

    render the list of slokas

--- src/components/Slokas.vue
+++ src/components/Slokas.vue
@@ -1,31 +1,26 @@
 <template>
-  <div class="hello">
+  <div class="slokas">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="http://chat.vuejs.org/" target="_blank" rel="noopener">Vue Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank" rel="noopener">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div v-for="sloka in slokas" :key="sloka.title">
+      <h3>{{sloka.title}}</h3>
+      <h4>{{sloka.description}}</h4>
+    </div>
   </div>
 </template>
 
 <script>
+import data from '../db.json';
+
 export default {
-  name: 'hello',
+  name: 'Slokas',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js PWA',
+      msg: 'Slokas of Yatiraja Saptati',
+      slokas: [],
     };
+  },
+  created() {
+    this.slokas = data;
   },
 };
 </script>
