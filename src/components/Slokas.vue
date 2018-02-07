cf58ab175744101150e1c08f8275b5ae8ab0f40b
pankaja-shree <pankajashree@gmail.com>
2018-02-07 15:16:09 +0530

    Move sloka from assets to static folder

--- src/components/Slokas.vue
+++ src/components/Slokas.vue
@@ -8,10 +8,7 @@
     <section>
       Playing: {{ selectedSloka.title }}
       <p>{{ selectedSloka.description }}</p>
-      <video width="320" height="240" controls>
-        <source :src="selectedSloka.videoUrl" type="video/mp4">
-      Your browser does not support the video tag.
-      </video>
+      <video :src="selectedSloka.videoUrl" controls></video>
     </section>
   </div>
 </template>
@@ -25,17 +22,17 @@ export default {
       selectedSloka: '',
       slokas: [
         {
-          videoUrl: '../assets/sloka01.mp4',
+          videoUrl: '/static/sloka01.mp4',
           title: 'Sloka 1',
           description: 'An array is a data structure that contains a group of elements. Typically these elements are all of the same data type, such as an integer or string. Arrays are commonly used in computer programs to organize data so that a related set of values can be easily sorted or searched.',
         },
         {
-          videoUrl: '../assets/sloka02.mp3',
+          videoUrl: '/static/sloka02.mp3',
           title: 'Sloka 2',
           description: 'Coding For Dummies is the perfect place to start! Explore the basics of HTML, CSS, JavaScript, Ruby, and Python and find out how to build great web applications.',
         },
         {
-          videoUrl: '../assets/sloka03.mp3',
+          videoUrl: '/static/sloka03.mp3',
           title: 'Sloka 3',
           description: 'For 10 years, Harry lives in the cupboard under the stairs and is subjected to cruel mistreatment by Aunt Petunia, Uncle Vernon and their son Dudley. On his 11th birthday, Harry receives a letter inviting him to study magic at the Hogwarts School of Witchcraft and Wizardry.',
         },
